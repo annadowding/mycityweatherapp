fcdc475415d402ce181c5304594f3da445bf2a5f
Anna Dowding <144890388+annadowding@users.noreply.github.com>
2023-12-05 16:43:21 +0000

    Applied JS to make API call for temperature

--- src/script.js
+++ src/script.js
@@ -1,12 +1,22 @@
+function refreshWeather(response) {
+  console.log(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityName = document.querySelector("#city");
+  cityName.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "c695b4fc90b605eea29b70ecbaft3f9o";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function citySearch(event) {
-
   event.preventDefault();
-
   let searchInput = document.querySelector("#formInput");
-  
-  let city = document.querySelector("#city");
-
-  city.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let formElement = document.querySelector("#form");
