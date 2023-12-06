9f4cd24641c7f2678480d2664ab0416fb34c9259
Anna Dowding <144890388+annadowding@users.noreply.github.com>
2023-12-06 09:48:02 +0000

    Added JS to update weather details when new city is entered

--- src/script.js
+++ src/script.js
@@ -3,8 +3,45 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityName = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windpseedElement = document.querySelector("#windspeed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
+  timeElement.innerHTML = formatDate(date);
+  windpseedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  descriptionElement.innerHTML = response.data.condition.description;
   cityName.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+
+    return `${day} ${hours}:${minutes}`;
+  }
 }
 
 function searchCity(city) {
@@ -21,3 +58,5 @@ function citySearch(event) {
 
 let formElement = document.querySelector("#form");
 formElement.addEventListener("submit", citySearch);
+
+searchCity("Paris");
